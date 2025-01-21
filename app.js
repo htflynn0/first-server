d768ab418401a1e33fe319b9ef149a50730175a4
Harrison <htflynn0@gmail.com>
2025-01-21 09:56:45 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 const logger = require("./logger.js")
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 
